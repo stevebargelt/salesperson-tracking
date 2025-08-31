f482be709df82bd1e1e2c56666d70428244e62ab
Steve Bargelt <steve@bargelt.com>
2025-08-31 06:02:15 -0700

    chore: cleanup generated JS artifacts and ignore future emits; fix Nx plugin error

--- apps/mobile/app/theme/timing.js
+++
@@ -1,7 +0,0 @@
-export const timing = {
-    /**
-     * The duration (ms) for quick animations.
-     */
-    quick: 300,
-};
-//# sourceMappingURL=timing.js.map
--- apps/mobile/app/utils/gestureHandler.native.js
+++
@@ -1,4 +0,0 @@
-// Only import react-native-gesture-handler on native platforms
-// https://reactnavigation.org/docs/drawer-navigator/#installation
-import "react-native-gesture-handler";
-//# sourceMappingURL=gestureHandler.native.js.map
