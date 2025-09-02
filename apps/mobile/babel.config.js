795afe278e4e25133e7dd43680c2e92a15bf8cd8
Steve Bargelt <steve@bargelt.com>
2025-09-01 17:22:31 -0700

    fix(mobile/ios-build): add Babel config and preset to resolve 'Cannot find module babel-preset-expo' during Xcode bundling

--- apps/mobile/babel.config.js
+++ apps/mobile/babel.config.js
@@ -1,7 +1,11 @@
-/** @type {import('@babel/core').TransformOptions} */
 module.exports = function (api) {
   api.cache(true)
   return {
-    presets: ["babel-preset-expo"],
+    presets: ['babel-preset-expo'],
+    plugins: [
+      // Keep this last per Reanimated docs
+      'react-native-reanimated/plugin',
+    ],
   }
 }
+
